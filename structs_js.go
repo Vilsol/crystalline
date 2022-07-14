508ba111f76d7f644a6b99d0d3307cd79d87367e
Vilsol <me@vil.so>
2022-07-15 02:22:45 +0300

    feat: add weak reference cache

--- structs_js.go
+++ structs_js.go
@@ -7,60 +7,75 @@ import (
 	"syscall/js"
 )
 
-var defineProperties js.Value
+var (
+	defineProperties js.Value
+	internalSet      js.Value
+)
+
+var weakCache *WeakCache
 
 func init() {
 	defineProperties = js.Global().Get("Object").Get("defineProperties")
+
+	js.Global().Get("eval").Invoke(`globalThis.__internal_set__ = (key) => {
+	return function() {
+		return this[key];
+	};
+}`)
+
+	internalSet = js.Global().Get("__internal_set__")
+
+	weakCache = NewWeak()
 }
 
 func convertStruct(value reflect.Value) (interface{}, error) {
-	out := make(map[string]interface{})
-	definitions := make(map[string]interface{})
+	return weakCache.Fetch(value.UnsafeAddr(), func() (interface{}, error) {
+		out := make(map[string]interface{})
+		definitions := make(map[string]interface{})
 
-	for i := 0; i < value.NumField(); i++ {
-		field := value.Field(i)
-		val, err := mapInternal(field)
-		if err != nil {
-			return nil, err
+		for i := 0; i < value.NumField(); i++ {
+			field := value.Field(i)
+			val, err := mapInternal(field)
+			if err != nil {
+				return nil, err
+			}
+
+			fieldName := value.Type().Field(i).Name
+			hiddenName := "_" + fieldName
+			out[hiddenName] = val
+
+			getFunc := internalSet.Invoke(hiddenName)
+
+			conv, err := jsToGo(field.Type())
+			if err != nil {
+				return nil, err
+			}
+
+			setFunc := js.FuncOf(func(this js.Value, args []js.Value) any {
+				this.Set(hiddenName, args[0])
+				field.Set(conv(args[0]))
+				return nil
+			})
+
+			definitions[fieldName] = js.ValueOf(map[string]interface{}{
+				"get": getFunc,
+				"set": setFunc,
+			})
 		}
 
-		fieldName := value.Type().Field(i).Name
-		hiddenName := "_" + fieldName
-		out[hiddenName] = val
-
-		getFunc := js.FuncOf(func(this js.Value, _ []js.Value) any {
-			return this.Get(hiddenName)
-		})
-
-		conv, err := jsToGo(field.Type())
-		if err != nil {
-			return nil, err
+		addr := value.Addr()
+		for i := 0; i < addr.NumMethod(); i++ {
+			val, err := mapInternal(addr.Method(i))
+			if err != nil {
+				return nil, err
+			}
+			out[addr.Type().Method(i).Name] = val
 		}
 
-		setFunc := js.FuncOf(func(this js.Value, args []js.Value) any {
-			this.Set(hiddenName, args[0])
-			field.Set(conv(args[0]))
-			return nil
-		})
+		obj := js.ValueOf(out)
 
-		definitions[fieldName] = js.ValueOf(map[string]interface{}{
-			"get": getFunc,
-			"set": setFunc,
-		})
-	}
+		defineProperties.Invoke(obj, definitions)
 
-	addr := value.Addr()
-	for i := 0; i < addr.NumMethod(); i++ {
-		val, err := mapInternal(addr.Method(i))
-		if err != nil {
-			return nil, err
-		}
-		out[addr.Type().Method(i).Name] = val
-	}
-
-	obj := js.ValueOf(out)
-
-	defineProperties.Invoke(obj, definitions)
-
-	return obj, nil
+		return obj, nil
+	})
 }
---
+++ weak_cache.go
@@ -0,0 +1,44 @@
+package crystalline
+
+import (
+	"runtime"
+)
+
+type fetch func() (interface{}, error)
+
+type WeakCache struct {
+	reachable map[uintptr]interface{}
+}
+
+func NewWeak() *WeakCache {
+	return &WeakCache{
+		reachable: make(map[uintptr]interface{}),
+	}
+}
+
+func (c *WeakCache) Fetch(key uintptr, fetch fetch) (interface{}, error) {
+	if found, ok := c.reachable[key]; ok {
+		return found, nil
+	}
+
+	value, err := fetch()
+	if err != nil {
+		return nil, err
+	}
+
+	runtime.SetFinalizer(&value, func(_ any) {
+		c.unref(key)
+	})
+
+	c.reachable[key] = value
+
+	return value, nil
+}
+
+func (c *WeakCache) unref(index uintptr) {
+	if _, ok := c.reachable[index]; !ok {
+		return
+	}
+
+	delete(c.reachable, index)
+}
