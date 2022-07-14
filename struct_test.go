55763658aa7cd22d439893d6baa6051d23db8455
Vilsol <me@vil.so>
2022-07-14 21:44:45 +0300

    chore: no pointer struct test

--- struct_test.go
+++ struct_test.go
@@ -20,3 +20,15 @@ func TestStructPointers(t *testing.T) {
 
 	testza.AssertEqual(t, "world", obj.FirstValue)
 }
+
+func TestStructNoPointers(t *testing.T) {
+	obj := FnSample{
+		FirstValue: "hello",
+	}
+
+	js.Global().Set("TestPointers", MapOrPanic(obj))
+
+	js.Global().Get("eval").Invoke("global.TestPointers.FirstValue = 'world'")
+
+	testza.AssertEqual(t, "hello", obj.FirstValue)
+}
